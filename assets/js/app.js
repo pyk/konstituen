dcc467edb7ce4f918fdd008d1d0245724ed5727b
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2014-05-25 12:55:56 +0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("konstituen", ['ngRoute', 'chieffancypants.loadingBar']);
+var app = angular.module("konstituen", ['ngRoute', 'ngResource','chieffancypants.loadingBar']);
 app.config(["$routeProvider",
   function($route) {
     $route
@@ -8,7 +8,7 @@ app.config(["$routeProvider",
       })
       .when("/beranda", {
         templateUrl: "assets/views/home.html",
-        controller: "HomeCtrl"
+        controller: "BeritaCtrl"
       })
       .when("/tentang", {
         templateUrl: "assets/views/tentang.html",
@@ -27,20 +27,23 @@ app.config(["$routeProvider",
       });
 }]);
 app.controller("BeritaCtrl",
-  ["$scope", "$http",
-  function ($s,$h) {
-    $s.getBerita = function () {
-    var key = "40381f1d8123102fd74d85aef44d70d5";
-    var url = "http://beritaapi.apiary-mock.com/berita";
-    $h.get(url, {params: {json: "get_recent_posts", apiKey: key, callback: "JSON_CALLBACK"}})
-      .success(function (data,status, header, config) {
-        console.log(data);
-      })
-      .error(function (data,status,header,config) {
-        console.log("error");
+  ["$scope", "$rootScope" , "$http", "$resource",
+  function ($s,$rs,$h,$r) {
+    $rs.hideLoader = false;
+    $s.display = true;
+    var url = "http://api.pemiluapi.org/berita?json=get_recent_posts&apiKey=fea6f7d9ec0b31e256a673114792cb17";
+    var BeritaAPI = $r(url, { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+
+    $s.getNews = function() {
+      BeritaAPI.get(function(data){
+          $s.data = data;
+          $rs.hideLoader = true;
+          $s.display = false;
+      }, function(err){
+          alert('request failed');
       });
-  };
-  $s.getBerita();
+    };
+    $s.getNews();
 }]);
 app.controller("GeoCtrl",
   ["$scope", "$http",
